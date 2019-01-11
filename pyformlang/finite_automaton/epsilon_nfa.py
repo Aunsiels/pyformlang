c61b49d46ae9c8a921ef5a4a0b9138b5a8f1be05
Julien Romero <romerojulien34@gmail.com>
2019-01-11 10:38:22 +0100

    correct inheritance

--- pyformlang/finite_automaton/epsilon_nfa.py
+++ pyformlang/finite_automaton/epsilon_nfa.py
@@ -2,15 +2,15 @@
 Nondeterminsitic Automaton with epsilon transitions
 """
 
-from typing import Set, Iterable
+from typing import Set, Iterable, AbstractSet
 
-from .nondeterministic_finite_automaton import NondeterministicFiniteAutomaton
 from .epsilon import Epsilon
 from .state import State
 from .symbol import Symbol
+from .nondeterministic_transition_function import NondeterministicTransitionFunction
 
 
-class EpsilonNFA(NondeterministicFiniteAutomaton):
+class EpsilonNFA(object):
     """ Represents an epsilon NFA
 
 
@@ -29,6 +29,195 @@ class EpsilonNFA(NondeterministicFiniteAutomaton):
         A set of final or accepting states. It is a subset of states.
 
     """
+
+    # pylint: disable=too-many-arguments
+    def __init__(self,
+                 states: AbstractSet[State] = None,
+                 input_symbols: AbstractSet[Symbol] = None,
+                 transition_function: NondeterministicTransitionFunction = None,
+                 start_state: AbstractSet[State] = None,
+                 final_states: AbstractSet[State] = None):
+        self._states = states or set()
+        self._input_symbols = input_symbols or set()
+        self._transition_function = transition_function or \
+            NondeterministicTransitionFunction()
+        self._start_state = start_state or set()
+        self._final_states = final_states or set()
+        for state in self._final_states:
+            if state is not None and state not in self._states:
+                self._states.add(start_state)
+        for state in self._start_state:
+            if state is not None and state not in self._states:
+                self._states.add(state)
+
+    def add_transition(self, s_from: State, symb_by: Symbol, s_to: State) -> int:
+        """ Adds a transition to the nfa
+
+        Parameters
+        ----------
+        s_from : :class:`~pyformlang.finite_automaton.State`
+            The source state
+        symb_by : :class:`~pyformlang.finite_automaton.Symbol`
+            The transition symbol
+        s_to : :class:`~pyformlang.finite_automaton.State`
+            The destination state
+
+
+        Returns
+        --------
+        done : int
+            Always 1
+
+        Raises
+        --------
+        DuplicateTransitionError
+            If the transition already exists
+        """
+        self._states.add(s_from)
+        self._states.add(s_to)
+        self._input_symbols.add(symb_by)
+        return self._transition_function.add_transition(s_from, symb_by, s_to)
+
+    def get_number_states(self) -> int:
+        """ Gives the total number of states
+
+        Returns
+        ----------
+        number_states : int
+            The number of states
+        """
+        return len(self._states)
+
+    def get_number_transitions(self) -> int:
+        """ Gives the number of transitions
+
+        Returns
+        ----------
+        n_transitions : int
+            The number of deterministic transitions
+
+        """
+        return self._transition_function.get_number_transitions()
+
+    def get_number_symbols(self) -> int:
+        """ Gives the total number of symbols
+
+        Returns
+        ----------
+        number_symbols : int
+            The number of symbols
+        """
+        return len(self._input_symbols)
+
+    def add_start_state(self, state: State) -> int:
+        """ Set an initial state
+
+        Parameters
+        -----------
+        state : :class:`~pyformlang.finite_automaton.State`
+            The new initial state
+
+        Returns
+        ----------
+        done : int
+            1 is correctly added
+        """
+        self._start_state.add(state)
+        self._states.add(state)
+        return 1
+
+    def remove_start_state(self, state: State) -> int:
+        """ remove an initial state
+
+        Parameters
+        -----------
+        state : :class:`~pyformlang.finite_automaton.State`
+            The new initial state
+
+        Returns
+        ----------
+        done : int
+            1 is correctly added
+        """
+        if state in self._start_state:
+            self._start_state.remove(state)
+            return 1
+        return 0
+
+    def add_final_state(self, state: State) -> int:
+        """ Adds a new final state
+
+        Parameters
+        -----------
+        state : :class:`~pyformlang.finite_automaton.State`
+            A new final state
+
+        Returns
+        ----------
+        done : int
+            1 is correctly added
+        """
+        self._final_states.add(state)
+        self._states.add(state)
+        return 1
+
+    def remove_final_state(self, state: State) -> int:
+        """ Remove a final state
+
+        Parameters
+        -----------
+        state : :class:`~pyformlang.finite_automaton.State`
+            A final state to remove
+
+        Returns
+        ----------
+        done : int
+            0 if it was not a final state, 1 otherwise
+        """
+        if self.is_final_state(state):
+            self._final_states.remove(state)
+            return 1
+        return 0
+
+    def is_final_state(self, state: State) -> bool:
+        """ Checks if a state is final
+
+        Parameters
+        -----------
+        state : :class:`~pyformlang.finite_automaton.State`
+            The state to check
+
+        Returns
+        ----------
+        is_final : bool
+            Whether the state is final or not
+        """
+        return state in self._final_states
+
+    def _get_next_states_iterable(self,
+                                  current_states: Iterable[State],
+                                  symbol: Symbol) \
+            -> Set[State]:
+        """ Gives the set of next states, starting from a set of states
+
+        Parameters
+        ----------
+        current_states : iterable of :class:`~pyformlang.finite_automaton.State`
+            The considered list of states
+        symbol : Symbol
+            The symbol of the link
+
+        Returns
+        ----------
+        next_states : set of :class:`~pyformlang.finite_automaton.State`
+            The next of resulting states
+        """
+        next_states = set()
+        for current_state in current_states:
+            next_states_temp = self._transition_function(current_state,
+                                                         symbol)
+            next_states = next_states.union(next_states_temp)
+        return next_states
 
     def accepts(self, word: Iterable[Symbol]) -> bool:
         """ Checks whether the epsilon nfa accepts a given word
@@ -92,3 +281,14 @@ class EpsilonNFA(NondeterministicFiniteAutomaton):
                     processed.add(conn_state)
                     to_process.append(conn_state)
         return processed
+
+    def is_deterministic(self) -> bool:
+        """ Checks whether an automaton is deterministic
+
+        Returns
+        ----------
+        is_deterministic : bool
+           Whether the automaton is deterministic
+        """
+        return len(self._start_state) <= 1 and \
+            self._transition_function.is_deterministic()
