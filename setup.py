1b02f78d809f6c0e879a54f25191e75a5dc62054
Julien Romero <romerojulien34@gmail.com>
2019-02-04 15:31:25 +0100

    begin cfg

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='pyformlang',
-    version='0.0.2',
+    version='0.0.3',
     #scripts=['pyformlang'] ,
     author="Julien Romero",
     author_email="romerojulien34@gmail.com",
