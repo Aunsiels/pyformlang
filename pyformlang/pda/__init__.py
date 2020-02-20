f41ddacdbe588b735de9fb2e626ca8299b0f74e7
Julien Romero <romerojulien34@gmail.com>
2020-02-20 11:14:21 +0100

    Documentation of push-down automata submodule

--- pyformlang/pda/__init__.py
+++ pyformlang/pda/__init__.py
@@ -1,3 +1,25 @@
+"""
+:mod:`pyformlang.pda`
+==================================
+
+This module deals with push-down automata.
+
+Available Classes
+-----------------
+
+PDA
+    A Push-Down Automaton
+State
+    A push-down automaton state
+Symbol
+    A push-down automaton symbol
+StackSymbol
+    A push-down automaton stack symbol
+Epsilon
+    A push-down automaton epsilon symbol
+
+"""
+
 from .pda import PDA
 from .state import State
 from .symbol import Symbol
