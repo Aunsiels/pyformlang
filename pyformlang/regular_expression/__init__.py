288c5fb57e829e2db90f263392a024a1e2cbb7b4
Julien Romero <romerojulien34@gmail.com>
2020-02-20 11:18:03 +0100

    Documentation of regex submodule

--- pyformlang/regular_expression/__init__.py
+++ pyformlang/regular_expression/__init__.py
@@ -1,5 +1,20 @@
 """
-A module to represent regular expressions
+:mod:`pyformlang.regular_expression`
+==================================
+
+This module deals with regular expression.
+
+By default, this module does not use the standard way to write regular
+expressions. Please read the documentation of Regex for more information.
+
+Available Classes
+-----------------
+
+Regex
+    A regular expression
+MisformedRegexError
+    An error occurring when the input regex is incorrect
+
 """
 
 
