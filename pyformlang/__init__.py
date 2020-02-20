6951138d9f8a6a99ac45a45b12f2f3b768699700
Julien Romero <romerojulien34@gmail.com>
2020-02-20 10:39:33 +0100

    write main module documentation

--- pyformlang/__init__.py
+++ pyformlang/__init__.py
@@ -1,5 +1,31 @@
 """
-pyformlang is a python module to perform operation on formal languages.
+Pyformlang
+==========
+
+Pyformlang is a python module to perform operation on formal languages.
+
+How to use the documentation
+----------------------------
+
+Documentation is available in two formats: docstrings directly
+in the code and a readthedocs website: https://pyformlang.readthedocs.io.
+
+Available subpackages
+---------------------
+regular_expression
+    Regular Expressions
+finite_automaton
+    Finite automata (deterministic, non-deterministic, with/without epsilon
+    transitions
+fst
+    Finite State Transducers
+cfg
+    Context-Free Grammar
+pda
+    Push-Down Automata
+Indexed Grammar
+    Indexed Grammar
+
 """
 
 __all__ = ["finite_automaton", "regular_expression"]
