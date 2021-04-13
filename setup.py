7302ae4c5ea337fc0196ee54c8b7a059beff984d
Julien Romero <romerojulien34@gmail.com>
2021-04-13 19:54:53 +0200

    New version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requirements = [
 
 setuptools.setup(
     name='pyformlang',
-    version='0.1.23',
+    version='0.1.24',
     #scripts=['pyformlang'] ,
     author="Julien Romero",
     author_email="romerojulien34@gmail.com",
